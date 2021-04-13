79199ea3e414c36cb7779be741a99dbb8a0b9291
Ron Gummich <ron.gummich@telekom.de>
2021-04-13 10:36:47 +0200

    trapping signals for graceful shutdown of app

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,24 @@ else
     echo "~/data is not empty"
 fi
 
-./app "$@"
+pid=0
+
+trap_handler() {
+    echo "trapped signal"
+    if [ $pid -ne 0 ]; then
+        kill -SIGTERM "$pid"
+        wait "$pid"
+    fi
+    exit 143; # 128 + 15 -- SIGTERM
+}
+
+trap 'kill ${!}; trap_handler' SIGINT SIGTERM
+
+./app "$@" &
+pid="$!"
+
+echo "App running with pid ${pid}"
+while true
+do
+  tail -f /dev/null & wait ${!}
+done
